6efb593ec548ca13cf7cd67de88188069f75974c
Thyago Salva <thyago.salva@bento.local>
2022-11-25 13:18:27 -0300

    jogo-forca

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const objetivo = ['B','R','A','S','I','L'];
+const letras = ['A','B','C','D','E','F','G','H',
+'I','J','K','L','M','N','O','P','Q','R','S','T','U',
+'V','W','X','Y','Z'];
+
+
+
+
+const verificaFimJogo = () => {
+    
+}
+
+const jogada = (l) => {
+    console.log(l)
+    verificaFimJogo()
+}
+
+const palavra = document.getElementById('palavra')
+objetivo.forEach((_,idx) => {
+    const letra = document.createElement('DIV')
+    letra.setAttribute('class','letra')
+    letra.setAttribute('id',`letra${idx}`)
+    palavra.appendChild(letra)
+})
+
+const botoes = document.getElementById('botoes')
+letras.forEach(l => {
+    const btn = document.createElement('BUTTON')
+    btn.setAttribute('type','button')
+    btn.innerHTML = l
+    btn.addEventListener('click', () => {
+        jogada(btn.innerHTML)
+    })
+    botoes.appendChild(btn)
+})
